3b3d86862f9a8ec7dead881dfc11bfbdd3e44b5b
BornGellote <icmanitoo@gmail.com>
2018-06-19 18:21:02 +0300

    test gem pony and test send email

---
+++ README.txt
@@ -0,0 +1,4 @@
+1. Need update gems.
+2. Install gem "Pony" - gem install pony.
+3. Allow unsafe applications in your mail settings.
+4. Done.
---
+++ send_mail.rb
@@ -0,0 +1,64 @@
+require 'pony'          #gem install pony
+require 'io/console'    #hide get in console
+
+def valid(email)
+    i = 0
+    while i != 1 do
+        if email.match?('[a-z0-9]+[_a-z0-9\.-]*[a-z0-9]+@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})')
+            puts 'Great Email correct!'
+            i += 1
+        else
+            puts 'Email not correct'
+            sleep 1
+            puts "Try it again"
+            email = STDIN.gets.chomp
+        end
+    end
+    # if email.match?('[a-z0-9]+[_a-z0-9\.-]*[a-z0-9]+@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})')
+    #     puts 'Great Email correct!'
+    # else
+    #     puts 'Email not correct'
+    #     sleep 1
+    #     puts "Try it again"
+    # end
+end
+puts "Enter your email"
+my_mail = STDIN.gets.chomp
+valid(my_mail)
+
+sleep 1
+
+puts "Enter password from #{my_mail} for send email"
+pass = STDIN.noecho(&:gets).chomp
+# pass = STDIN.gets.chomp
+
+sleep 1
+
+puts "To whom to send a letter?"
+send_to = STDIN.gets.chomp
+valid(send_to)
+
+sleep 1
+puts "Write the text of the letter"
+body = STDIN.gets.chomp
+
+Pony.mail({
+    :subject  => "hello from the ruby",
+    :body     => body,
+    :to       => send_to,
+    :from     => my_mail,
+
+    :via => :smtp,
+    :via_options => {
+        :address              => 'smtp.gmail.com',
+        :port                 => '587',
+        :enable_starttls_auto => true,
+        :user_name            => my_mail,
+        :password             => pass,
+        :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+        :domain               => "localhost.localdomain" #the HELO domain provided by the client to the server
+    }
+})
+
+sleep 1
+puts "Email successfully sent"
