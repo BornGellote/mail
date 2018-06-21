2223dcbfa8b2dfa54cc7a9f7934af20b7cbb018f
BornGellote <icmanitoo@gmail.com>
2018-06-21 12:30:17 +0300

    added rescue

--- send_mail.rb
+++ send_mail.rb
@@ -42,23 +42,47 @@ sleep 1
 puts "Write the text of the letter"
 body = STDIN.gets.chomp
 
-Pony.mail({
-    :subject  => "hello from the ruby",
-    :body     => body,
-    :to       => send_to,
-    :from     => my_mail,
+def send_mail(body, send_to, my_mail, pass)
+    Pony.mail({
+                  :subject  => "hello from the ruby",
+                  :body     => body,
+                  :to       => send_to,
+                  :from     => my_mail,
 
-    :via => :smtp,
-    :via_options => {
-        :address              => 'smtp.gmail.com',
-        :port                 => '587',
-        :enable_starttls_auto => true,
-        :user_name            => my_mail,
-        :password             => pass,
-        :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-        :domain               => "localhost.localdomain" #the HELO domain provided by the client to the server
-    }
-})
+                  :via => :smtp,
+                  :via_options => {
+                      :address              => 'smtp.gmail.com',
+                      :port                 => '587',
+                      :enable_starttls_auto => true,
+                      :user_name            => my_mail,
+                      :password             => pass,
+                      :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+                      :domain               => "localhost.localdomain" #the HELO domain provided by the client to the server
+                  }
+              })
 
-sleep 1
-puts "Email successfully sent"
+    sleep 1
+    puts "Email successfully sent"
+end
+
+begin
+    send_mail(body, send_to, my_mail, pass)
+
+rescue SocketError
+  sleep 1
+  puts "I can't connect to server :)"
+rescue Net::SMTPSyntaxError => error
+  sleep 1
+  puts "You wrote not correct params email: " + error.message
+rescue Net::SMTPAuthenticationError => error #если произошла ошибка
+  sleep 1
+  puts "Something went wrong. Email could not be sent!!!"
+  sleep 1
+  puts "Not wrong password from your - #{my_mail} email. Try again!"
+  pass = STDIN.noecho(&:gets).chomp
+  send_mail(body, send_to, my_mail, pass)
+# ensure #код выполниться в любом случае
+#     sleep 1
+#     puts "Yeahooo!!!"
+end
+
